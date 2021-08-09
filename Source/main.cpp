a08707112b75355ae3742706282b3817c6c240e2
Dmytro Shestakov <altais@ukr.net>
2021-08-09 23:14:35 +0300

    Cleanup main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,69 +1,13 @@
-#include "scmRTOS.h"
-#include "gpio.h"
+#include "scm_utils.h"
 #include "timers.h"
 #include "pinlist.h"
-#include "uart.h"
-#include "i2c.h"
 
 using namespace Mcudrv;
-typedef Uarts::UartIrq<> Uart;
 
-typedef Twis::SoftTwi<Twis::Standard, Pb4, Pb5> I2c;
-typedef Twis::Lm75<I2c> Lm75;
-const uint8_t lm75_devAddr = 7;
-
-template void Uart::TxISR();
-template void Uart::RxISR();
-
-typedef OS::process<OS::pr0, 100> TProc1;
-template<> void TProc1::exec();
-
-static TProc1 proc1;
-
-template<> void TProc1::exec()
+SCM_TASK(ButtonHandler, OS::pr0, 200)
 {
     while(true) {
-        sleep(200);
-        Pd2::Toggle();
-        int16_t val = Lm75::Read(lm75_devAddr);
-        Uart::Puts(val >> 1);
-        Uart::Putch('.');
-        Uart::Putch((val & 0x01) ? '5' : '0');
-        Uart::Newline();
-    }
-}
-
-typedef OS::process<OS::pr1, 100> TProc2;
-template<> void TProc2::exec();
-
-static TProc2 proc2;
-
-template<> void TProc2::exec()
-{
-    using namespace T1;
-    while(true) {
-        uint8_t ch;
-        if(Uart::Getch(ch)) {
-            switch(ch) {
-            case 'a':
-                Timer1::GetCompareByte<Ch1>() -= 1;
-                break;
-            case 's':
-                Timer1::GetCompareByte<Ch2>() -= 1;
-                break;
-            case 'q':
-                Timer1::GetCompareByte<Ch1>() += 1;
-                break;
-            case 'w':
-                Timer1::GetCompareByte<Ch2>() += 1;
-                break;
-            }
-            Uart::Puts(Timer1::ReadCompareByte<Ch1>(), 10);
-            Uart::Putch(' ');
-            Uart::Puts(Timer1::ReadCompareByte<Ch2>(), 10);
-            Uart::Newline();
-        }
-        sleep(1);
+        sleep(MS2ST(50));
     }
 }
 
@@ -72,7 +16,7 @@ static void InitPwm()
     using namespace T1;
     GpioC::SetConfig<P6 | P7, GpioBase::Out_PushPull_fast>();
     Timer1::Init(1, ARPE | CEN);
-    Timer1::WriteAutoReload(80);
+    Timer1::WriteAutoReload(80); // PWM 25kHz
     Timer1::SetChannelCfg<Ch1, Output, static_cast<ChannelCfgOut>(Out_PWM_Mode1 | Out_PreloadEnable)>();
     Timer1::SetChannelCfg<Ch2, Output, static_cast<ChannelCfgOut>(Out_PWM_Mode1 | Out_PreloadEnable)>();
     Timer1::ChannelEnable<Ch1>();
@@ -87,13 +31,11 @@ static void InitPeripherals()
     GpioC::WriteConfig<0xFF, GpioBase::In_Pullup>();
     GpioD::WriteConfig<0xFF, GpioBase::In_Pullup>();
 
-    T4::Timer4::Init(T4::Div_32, T4::Cfg(T4::ARPE | T4::CEN)); // 122Hz
+    T4::Timer4::Init(T4::Div_32, T4::Cfg(T4::ARPE | T4::CEN)); // Systick = 122Hz
     T4::Timer4::EnableInterrupt();
 
     Pd2::SetConfig<GpioBase::Out_PushPull>();
 
-    Uart::Init<Uarts::DefaultCfg, 57600>();
-    I2c::Init();
     InitPwm();
 }
 
